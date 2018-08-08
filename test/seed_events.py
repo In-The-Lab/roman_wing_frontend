fb9e425290463d1e7854251ff19b74ebd6ce33b9
Dant86 <vedantdpathak@iCloud.com>
2018-08-08 10:52:10 -0500

    added admin seed

--- test/seed_events.py
+++ test/seed_events.py
@@ -3,6 +3,15 @@ from db.dbutils import get_db_config, get_db_connection
 cnx, cursor = get_db_connection(*get_db_config())
 url = "https://i.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg"
 
+cmd = (
+    "INSERT INTO users "
+    "(first_name, last_name, email, is_admin, date_created) "
+    "VALUES (\'sample\', \'admin\', \'sample@admin.com\', "
+    "TRUE, CURDATE())"
+)
+cursor.execute(cmd)
+cnx.commit()
+
 for i in range(20):
     str_ = "seed post {}".format(i)
     cmd = (
@@ -10,7 +19,7 @@ for i in range(20):
         "(creator_id, title, description, body, date_created, "
         "thumbnail_url, is_authorized) "
         "VALUES ({}, \'{}\', \'{}\', \'{}\', CURDATE(), \'{}\', TRUE)"
-        .format(2, str_, str_, str_, url)
+        .format(1, str_, str_, str_, url)
     )
     cursor.execute(cmd)
     cnx.commit()
